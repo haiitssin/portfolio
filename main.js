4d30c01286da3c967192506d53b92d5fe32f26ee
Anna <146865409+haiitssin@users.noreply.github.com>
2023-11-21 10:37:02 -0700

    Connecting javascript
    Working on next phase here

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-alert ('Hello World');
-
+//global "scope", variables avalible to all//
 let lastname = "Liu"; 
 const firstname = "Anna"; //const is so that it does change
-firstname = "patrick";
 let age = "25"; 
-console.log(typeof age);
-console.log(firstname);
 
+let namelist = "huh";
+const fullname = (first, last) => {
+    console.log("first, last");
+    console.log(first + "s favourite drink is milk")
+    console.log(`${first}'s favourite drink is milk`) //does same thing but anotehr way to write it//
+let veggie = "cucumber"; //local scope, only used withint he brackets//
+}
+//if else statements//
+if (firstname){
+    console.log("omg its true!")
+}
+const nameslist = "9";
+namesList.pop();
+fullname("Stephen", "P");
+fullname("Magic", "Meow Meow");
+
+//for loop//
+for(let counter = 0; counter < 4; counter = counter + 1){
+    fullname(namelist,{counter});
+
+}
+//whtie loop//
+let j = 0;
+while (j < namelist.length) {
+    fullname(namesList(j)); 
+    
+} //idk what's happening T^T
