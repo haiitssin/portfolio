25bc77d19f8655714782e1bb62a8c141eff3f9bc
Anna <146865409+haiitssin@users.noreply.github.com>
2023-11-21 09:59:51 -0700

    Final changes for css
    Ignore the js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 alert ('Hello World');
 
+let lastname = "Liu"; 
 const firstname = "Anna"; //const is so that it does change
 firstname = "patrick";
+let age = "25"; 
+console.log(typeof age);
 console.log(firstname);
 
